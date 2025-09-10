3424ddb7c05129d866e66232288a1ef4ae0c5c30
mahina2007 <tojiyevamahina.2007@gmail.com>
2025-09-10 19:02:51 +0500

    The configutarion is ready to work

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -7,10 +7,10 @@ def about_page_view(request):
     return render(request, 'auth/about-us.html')
 
 def contact_page_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'auth/contact.html')
 
 def blog_detail_page_view(request):
-    return render(request, 'blog-detail.html')
+    return render(request, 'blogs/blog-detail.html')
 
 def blog_list_page_view(request):
-    return render(request, 'blog-list-sidebar-left.html')
+    return render(request, 'blogs/blog-list-sidebar-left.html')
